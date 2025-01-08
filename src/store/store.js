98ca504b34ec4f94e9b7456706a3afb0931c4e73
Mayank <mayank.rhl2004@gmail.com>
2025-01-08 17:02:37 +0530

    final

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,4 @@
-// import {configureStore} from '@reduxjs/toolkit';
-// import authSlice from './authSlice';
 
-// const store = configureStore({
-//     reducer: {
-//         auth : authSlice,
-//         //TODO: add more slices here for posts
-//     }
-// });
-
-
-// export default store;
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import authSlice from './authSlice';
-
-// const store = configureStore({
-//     reducer: {
-//         auth: authSlice,
-//         //TODO: add more slices here for posts
-//     }
-// });
-
-// export default store;
 
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
